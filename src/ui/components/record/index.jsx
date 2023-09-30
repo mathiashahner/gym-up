751c4e6ff88699680609c0d8a4c0ded5de98b6fd
Mathias Hahner <mathias_hahner@hotmail.com>
2023-09-30 19:39:58 -0300

    Created editing animation

--- src/ui/components/record/index.jsx
+++ src/ui/components/record/index.jsx
@@ -1,16 +1,28 @@
 import styles from './styles'
 
+import { useEffect } from 'react'
 import { Text, View } from 'react-native'
+import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming } from 'react-native-reanimated'
 
 export const Record = ({ title, value, icon }) => {
+  const sharedValue = useSharedValue(-1.5)
+
+  useEffect(() => {
+    sharedValue.value = withRepeat(withTiming(1), -1, true)
+  }, [])
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${sharedValue.value * 1.5}deg` }],
+  }))
+
   return (
-    <View style={styles.record}>
+    <Animated.View style={[styles.record, animatedStyle]}>
       {icon}
 
       <View>
         <Text style={styles.recordTitle}>{title}</Text>
         <Text style={styles.recordValue}>{value}</Text>
       </View>
-    </View>
+    </Animated.View>
   )
 }
